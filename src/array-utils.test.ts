ee36a670257de0fca67255616b321a5692e78541
alexandre-dev28 <axel.business29@gmail.com>
2023-12-08 11:16:25 +0200

    initialise my project

---
+++ src/array-utils.test.ts
@@ -0,0 +1,58 @@
+import { expect, test, describe } from "vitest";
+import "./array-utils";
+
+describe("Should test the bubbleSort method", () => {
+  const myArray = [1, 5, 4, 3, 50, 200, 88];
+  const isSorted = (array: Array<any>) => {
+    return (
+      array.every((v, i) => i === 0 || v <= array[i - 1]) ||
+      array.every((v, i) => i === 0 || v >= array[i - 1])
+    );
+  };
+
+  test("Verify that the bubbleSort method is present on the array object", () => {
+    expect("bubbleSort" in myArray).true;
+  });
+  test("test if all elements are sorted", () => {
+    expect(isSorted(myArray.bubbleSort())).true;
+  });
+});
+describe("Should test the sumNumbers method", () => {
+  const myArray = [50, 200];
+
+  test("Verify that the sumNumbers method is present on the array object", () => {
+    expect("sumNumbers" in myArray).true;
+  });
+  test("test if all the sum will be 250", () => {
+    expect(myArray.sumNumbers()).toEqual(250);
+  });
+  test("test if it will throw error when using other than numbers", () => {
+    expect(() => ["mango", "pineapple"].sumNumbers()).toThrowError();
+  });
+});
+describe("Should test the sumNumbers method", () => {
+  type Users = {
+    dateCreation: Date;
+    groupe: string;
+    name: string;
+  };
+  const listOfUsers: Users[] = [
+    { dateCreation: new Date(), groupe: "Facebook", name: "John" },
+    { dateCreation: new Date(), groupe: "GDG", name: "Doe" },
+    { dateCreation: new Date(), groupe: "GDG", name: "Alexa" },
+  ];
+  const groupeByName = listOfUsers.groupBy("name");
+  const groupByGroupe = listOfUsers.groupBy("groupe");
+
+  test("Verify that the groupBy method is present on the array object", () => {
+    expect("groupBy" in listOfUsers).true;
+  });
+  test("test if it will find 1 group of john", () => {
+    const alexandres = groupeByName["John"] as Array<Users>;
+    expect(alexandres.length).toEqual(1);
+  });
+  test("Should return undefined for not found elements", () => {
+    const gdgs = groupByGroupe["RandomElement"] as Array<Users>;
+    expect(gdgs).toBeUndefined();
+  });
+});
---
+++ src/array-utils.ts
@@ -0,0 +1,52 @@
+class ArrayOperations {
+  static sumArray(arrayOfNumbers: number[]): number {
+    return arrayOfNumbers.reduce(function (accumulator, currentValue) {
+      return accumulator + currentValue;
+    }, 0);
+  }
+  static bubbleSort(array: number[]): number[] {
+    array = array.slice(); // creates a copy of the array
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          let swap = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = swap;
+        }
+      }
+    }
+    return array;
+  }
+
+  static groupBy<T, K extends keyof T>(key: K, arrayElement: T[]): Record<string, T[]> {
+    return arrayElement.reduce((acc: Record<string, T[]>, obj: T) => {
+      const groupByKey = obj[key] as any;
+      if (!acc[groupByKey]) {
+        acc[groupByKey] = [];
+      }
+      acc[groupByKey].push(obj);
+      return acc;
+    }, {} as Record<string, T[]>);
+  }
+}
+
+interface Array<T> {
+  sumNumbers(): number;
+  bubbleSort(): number[];
+  groupBy<K extends keyof T>(key: K): Record<string, T[]>;
+}
+
+Array.prototype.groupBy = function <T, K extends keyof T>(key: K): Record<string, T[]> {
+  return ArrayOperations.groupBy(key, this);
+};
+
+Array.prototype.bubbleSort = function () {
+  return ArrayOperations.bubbleSort(this);
+};
+Array.prototype.sumNumbers = function () {
+  if (Array.isArray(this) && this.every((element) => typeof element === "number")) {
+    return ArrayOperations.sumArray(this);
+  }
+  throw new Error("this method accepts only numbers");
+};
